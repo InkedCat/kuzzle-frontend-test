9b6c3fc2e4a19c5eb9471f86aaa4d58a50867db2
InkedCat <146424693+InkedCat@users.noreply.github.com>
2024-03-20 20:01:35 +0100

    feat: add message chatbox component

---
+++ src/components/ChatBox.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import MessageBubble from '@/components/MessageBubble.vue';
+import type { ViewMessage } from '@/interfaces/Message';
+
+const props = defineProps<{messages: ViewMessage[], author: string}>();
+
+</script>
+
+<template>
+    <div id="chatbox" class="flex flex-col gap-3 flex-grow overflow-auto">
+        <TransitionGroup appear
+            enter-active-class="transition-all duration-500"
+            enter-from-class="opacity-0 -translate-x-4">
+            <MessageBubble v-for="(message) in props.messages" 
+                :key="message.timestamp" 
+                :author="message.author" 
+                :content="message.content" 
+                :timestamp="message.timestamp"
+                :isAuthor="author === message.author">
+            </MessageBubble>
+        </TransitionGroup>
+    </div>
+</template>
