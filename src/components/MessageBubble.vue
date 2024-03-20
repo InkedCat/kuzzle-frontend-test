612364c3983e9224057475e861761233964cf0d2
InkedCat <146424693+InkedCat@users.noreply.github.com>
2024-03-20 19:58:51 +0100

    feat: add message bubble component

---
+++ src/components/MessageBubble.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{author: string, content:string, timestamp: number, isAuthor: boolean}>();
+
+const date = new Date(props.timestamp);
+
+const formatedDate = `${date.getHours()}:${date.getMinutes()}`;
+</script>
+
+<template>
+    <div :class="{'ml-auto bg-primary-blue text-white': isAuthor}" class="flex flex-col gap-1 w-fit px-6 py-1 bg-[#E0E0E0] rounded-3xl">
+        <div class="flex gap-4">
+            <p class="italic font-bold">
+                {{props.author}}
+            </p>
+            <p>
+                {{props.content}}
+            </p>
+        </div>
+        <p :class="{'text-white': isAuthor}" class="text-end text-xs font-extralight text-secondary-background">
+            {{formatedDate}}
+        </p>
+    </div>
+</template>
