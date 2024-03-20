3f5f633163b03e7f9145a50029eab1eefa433995
InkedCat <146424693+InkedCat@users.noreply.github.com>
2024-03-20 20:26:34 +0100

    feat: add username handling to chat

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import Signup from '@/components/Signup.vue';
 import { Kuzzle } from 'kuzzle-sdk';
 import WebSocketProtocol from 'kuzzle-sdk/src/protocols/WebSocket';
-import { onMounted } from 'vue';
+import { onMounted, ref, type Ref } from 'vue';
 
 let client : Kuzzle;
+
+const author = ref(localStorage.getItem('author') || '');
+
 
 async function initConnection() {
     client = new Kuzzle(new WebSocketProtocol('localhost'));
@@ -11,9 +15,25 @@ async function initConnection() {
 
 }
 
+async function registerAuthor(newAuthor: string) {
+    author.value = newAuthor;
+    localStorage.setItem('author', newAuthor);
+
+    await initConnection();
+}
+
+function unregisterAuthor() {
+    author.value = '';
+    localStorage.removeItem('author');
+
+    client.disconnect();
+    messages.value = [];
+}
+
+
 onMounted(async () => {
     try {
-        await initConnection();
+        if(author.value) await initConnection();
     } catch (error) {
         console.error(error);
     }
@@ -21,4 +41,6 @@ onMounted(async () => {
 </script>
 
 <template>
+    <Signup v-if="!author" @author-submit="registerAuthor"/>
+
 </template>
