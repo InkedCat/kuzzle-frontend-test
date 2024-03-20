3f0d4455f19a0795abc274536eef99532b545236
InkedCat <146424693+InkedCat@users.noreply.github.com>
2024-03-20 20:20:25 +0100

    feat: add Kuzzle Backend connection to chat view

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,2 +1,24 @@
+<script setup lang="ts">
+import { Kuzzle } from 'kuzzle-sdk';
+import WebSocketProtocol from 'kuzzle-sdk/src/protocols/WebSocket';
+import { onMounted } from 'vue';
+
+let client : Kuzzle;
+
+async function initConnection() {
+    client = new Kuzzle(new WebSocketProtocol('localhost'));
+    await client.connect();
+
+}
+
+onMounted(async () => {
+    try {
+        await initConnection();
+    } catch (error) {
+        console.error(error);
+    }
+})
+</script>
+
 <template>
 </template>
