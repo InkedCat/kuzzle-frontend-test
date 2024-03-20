91a5230ca3282b23e6b8bc948d405687b04ba8ea
InkedCat <146424693+InkedCat@users.noreply.github.com>
2024-03-20 20:10:11 +0100

    feat: add Chat view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue'
+import Home from '../views/Home.vue';
+import Chat from '../views/Chat.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,12 @@ const router = createRouter({
       name: 'home',
       component: Home
     },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: Chat
+    },
+  ]
 })
 
 export default router
