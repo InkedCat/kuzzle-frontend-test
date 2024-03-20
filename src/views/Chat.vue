36b6121278e0ef000ee2236277f9ed8b0cba7ad4
InkedCat <146424693+InkedCat@users.noreply.github.com>
2024-03-20 20:29:49 +0100

    feat: add messages request when chat load

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import ChatBox from '@/components/ChatBox.vue';
+import MessageForm from '@/components/MessageForm.vue';
 import Signup from '@/components/Signup.vue';
+import { getMessages, postMessage } from '@/libs/ChatApi';
+import { setFlashMessage } from '@/libs/FlashStore';
+import axios from 'axios';
 import { Kuzzle } from 'kuzzle-sdk';
 import WebSocketProtocol from 'kuzzle-sdk/src/protocols/WebSocket';
 import { onMounted, ref, type Ref } from 'vue';
 
 let client : Kuzzle;
 
+const messages : Ref<ViewMessage[]> = ref([]);
 const author = ref(localStorage.getItem('author') || '');
 
 
@@ -15,11 +21,34 @@ async function initConnection() {
 
 }
 
+async function submitMessage(message: string) {
+    if(!author) return;
+
+    const postRequest = await postMessage(message.trim(), author.value);
+
+    if(axios.isAxiosError(postRequest) && postRequest.response) {
+        setFlashMessage(postRequest.response.data.error.message)
+    }
+}
+
+async function updateMessages() {
+    try {
+        const messageResponse = await getMessages();
+        console.log(messageResponse);
+        if(!axios.isAxiosError(messageResponse)) {
+            messages.value = messageResponse
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 async function registerAuthor(newAuthor: string) {
     author.value = newAuthor;
     localStorage.setItem('author', newAuthor);
 
     await initConnection();
+    await updateMessages();
 }
 
 function unregisterAuthor() {
@@ -34,6 +63,8 @@ function unregisterAuthor() {
 onMounted(async () => {
     try {
         if(author.value) await initConnection();
+
+        await updateMessages();
     } catch (error) {
         console.error(error);
     }
@@ -43,4 +74,9 @@ onMounted(async () => {
 <template>
     <Signup v-if="!author" @author-submit="registerAuthor"/>
 
+    <div v-else class="flex flex-col gap-4 w-full h-[75vh] px-16 py-6 bg-[#ededed] rounded-xl">
+        <button @click="unregisterAuthor" class="bg-gray-300 rounded-3xl px-4 py-2">Disconnect</button>
+        <ChatBox :messages="messages" :author="author"/>
+        <MessageForm @message-submit="submitMessage"/>
+    </div>
 </template>
